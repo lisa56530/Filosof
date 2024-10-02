71f9097c74195fe3a343a20686938a68dd85af7d
lisa56530 <145032722+lisa56530@users.noreply.github.com>
2024-10-02 10:39:00 +0200

    Update ChopStick.java

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,22 +1,36 @@
 package diningphilosophers;
 
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
+
 public class ChopStick {
     private static int stickCount = 0;
     private boolean iAmFree = true;
+    // private int delay = 0 ;
     private final int myNumber;
 
     public ChopStick() {
         myNumber = ++stickCount;
+
     }
 
-    synchronized public void take() throws InterruptedException {
-        while (!iAmFree) {
-            wait();
+    private final Lock verrou = new ReentrantLock();
+
+    private final Condition libre = verrou.newCondition();
+
+    public boolean tryTake() throws InterruptedException {
+        verrou.lock();
+        try {
+            while (!iAmFree) {
+                libre.await();
+            }
+            iAmFree = false;
+            System.out.println("Stick " + myNumber + " c'est déjà pris !");
+            return true;
+        } finally {
+            verrou.unlock();
         }
-        // assert iAmFree;
-        iAmFree = false;
-        System.out.println("baguette " + myNumber + " prise");
-        // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
     }
 
     synchronized public void release() {
@@ -26,9 +40,39 @@ public class ChopStick {
         notifyAll(); // On prévient ceux qui attendent que la baguette soit libre
     }
 
-   @Override
+    @Override
     public String toString() {
         return "baguette #" + myNumber;
     }
-    
+
 }
+
+/* 
+
+    synchronized public boolean take() throws InterruptedException {
+        if (!iAmFree) {
+            try {
+                wait(delay);
+            } catch (InterruptedException miam) {
+            }
+            if (!iAmFree) {
+                System.out.println("Stick " + myNumber + " Prend donc mais relâche après !");
+                return false;
+            }
+        } else
+            iAmFree = false;
+        System.out.println("Stick " + myNumber + " C'est pris !");
+        return true;
+    }
+
+    public void release() throws InterruptedException{
+                verrou.lock();
+                try {
+                    iAmFree = true;
+                    System.out.println("Stick " + myNumber + " Released");
+                    libre.signalAll();
+                } finally {
+                    verrou.unlock();
+                }
+            }
+ */
